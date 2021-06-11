ae0dd11100cc9fa1f27c9be062636689a26aaa82
OmarBish <omar.f.bishtawi@gmail.com>
2021-06-11 23:14:10 +0300

    factoryform upadte for hungaring problem

--- src/views/Home/FactoryForm.vue
+++ src/views/Home/FactoryForm.vue
@@ -42,28 +42,82 @@
             <hr class="mb-3" />
           </b-col>
         </b-row>
-        <b-row ref="row" v-if="replacementMode">
-          <!-- Item Name -->
-          <b-col md="4">
-            <b-form-group
-              :label="`Maximum Factory Capacity `"
-              label-for="item-max-ptw"
-            >
-              <b-form-input
-                id="item-max-ptw"
-                type="number"
-                placeholder="32"
-                :value="max_capacity"
-                @input="setMaxCapacity($event)"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col cols="12">
-            <hr class="mb-3" />
-          </b-col>
-        </b-row>
+        <!-- Row Loop -->
+        <div v-if="replacementMode">
+          <b-row
+            v-for="(item, index) in items"
+            :id="item.id"
+            :key="item.id"
+            ref="row"
+          >
+            <!-- Item Name -->
+            <b-col md="2">
+              <b-form-group label="Storage Name" label-for="item-name">
+                <b-form-input
+                  id="item-name"
+                  type="text"
+                  placeholder="EX: Nasra storage"
+                  :value="item.name"
+                  @input="setName($event, item, index)"
+                />
+              </b-form-group>
+            </b-col>
+
+            <!-- Capacity -->
+            <b-col md="2">
+              <b-form-group label="Max Capacity" label-for="capacity">
+                <b-form-input
+                  id="capacity"
+                  type="number"
+                  placeholder="32"
+                  :value="item.capacity"
+                  @input="setCapacity($event, item, index)"
+                />
+              </b-form-group>
+            </b-col>
+
+            <!-- Rent -->
+            <b-col md="2">
+              <b-form-group label="Rent" label-for="rent">
+                <b-form-input
+                  id="rent"
+                  type="number"
+                  placeholder="32"
+                  :value="item.rent"
+                  @input="setRent($event, item, index)"
+                />
+              </b-form-group>
+            </b-col>
+
+            <!-- Remove Button -->
+            <b-col lg="2" md="3" class="mb-50">
+              <b-button
+                v-ripple.400="'rgba(234, 84, 85, 0.15)'"
+                variant="outline-danger"
+                class="mt-0 mt-md-2"
+                @click="removeItem(index)"
+              >
+                <feather-icon icon="XIcon" class="mr-25" />
+                <span>Delete</span>
+              </b-button>
+            </b-col>
+
+            <!-- Seperator -->
+            <b-col cols="12">
+              <hr />
+            </b-col>
+          </b-row>
+        </div>
       </b-form>
     </div>
+    <b-button
+      v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+      variant="primary"
+      @click="repeateAgain"
+    >
+      <feather-icon icon="PlusIcon" class="mr-25" />
+      <span>Add New</span>
+    </b-button>
   </b-card>
 </template>
 
@@ -76,6 +130,7 @@ import {
   BRow,
   BCol,
   BCard,
+  BButton,
 } from "bootstrap-vue";
 import { heightTransition } from "@core/mixins/ui/transition";
 import Ripple from "vue-ripple-directive";
@@ -88,6 +143,7 @@ export default {
     BCol,
     BFormGroup,
     BFormInput,
+    BButton,
   },
   directives: {
     Ripple,
@@ -111,9 +167,12 @@ export default {
     replacementMode() {
       return this.$store.state.lp.replacementMode;
     },
+    items() {
+      return this.$store.getters["lp/storages"];
+    },
   },
   mounted() {
-    // this.initTrHeight();
+    this.initTrHeight();
   },
   created() {
     window.addEventListener("resize", this.initTrHeight);
@@ -122,6 +181,30 @@ export default {
     window.removeEventListener("resize", this.initTrHeight);
   },
   methods: {
+    setName(value, item, index) {
+      const tempItem = item;
+      tempItem.name = value;
+      this.$store.commit("lp/SET_STORAGE_ITEM", {
+        index,
+        storage: tempItem,
+      });
+    },
+    setRent(value, item, index) {
+      const tempItem = item;
+      tempItem.rent = value;
+      this.$store.commit("lp/SET_STORAGE_ITEM", {
+        index,
+        storage: tempItem,
+      });
+    },
+    setCapacity(value, item, index) {
+      const tempItem = item;
+      tempItem.capacity = value;
+      this.$store.commit("lp/SET_STORAGE_ITEM", {
+        index,
+        storage: tempItem,
+      });
+    },
     setMaxPtm(value) {
       this.$store.commit("lp/SET_MAX_PTM", value);
     },
@@ -130,6 +213,19 @@ export default {
     },
     setMaxCapacity(value) {
       this.$store.commit("lp/SET_MAX_CAPACITY", value);
+    },
+    repeateAgain() {
+      this.$store.commit("lp/ADD_NEW_STORAGE", {
+        id: (this.nextTodoId += this.nextTodoId),
+      });
+
+      this.$nextTick(() => {
+        this.trAddHeight(this.$refs.row[0].offsetHeight);
+      });
+    },
+    removeItem(index) {
+      this.$store.commit("lp/REMOVE_STORAGE", index);
+      this.trTrimHeight(this.$refs.row[0].offsetHeight);
     },
     initTrHeight() {
       this.trSetHeight(null);
